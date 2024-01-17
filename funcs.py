b5f42193fd9e7a19f329b0567dd89c720a57d26d
AlexandrKruglov <krulalex84@gmail.com>
2024-01-18 01:31:44 +0300

    difference_of_two_numbers

--- funcs.py
+++ funcs.py
@@ -1,3 +1,8 @@
 def adding_two_numbers(num_1, num_2):
     num_sum = num_1 + num_2
     return num_sum
+
+
+def difference_of_two_numbers(num_1, num_2):
+    num_diff = num_1 - num_2
+    return num_diff
